7ca772be3c26525ce4e4442274032ff9e9ebeffe
Roman Gusev <rgusev@hwdtech.com>
2016-03-31 21:06:12 +0600

    Improve rsa keygen speed

--- rsa.py
+++ rsa.py
@@ -37,37 +37,21 @@ class PrivateKey(object):
 
 
 def get_key_pair(length):
-    def get_d(e, phi):
-        d = 0
-        while True:
-            d += 1
-            if (d * e) % phi == 1:
-                return d
-
     def get_e(phi):
-        assert phi > 3
-
-        coprimes = []
-        for num in range(3, phi):
-            if gcd(num, phi) == 1:
-                coprimes.append(num)
-
-        return random.choice(coprimes)
+        while True:
+            x = random.randint(3, phi)
+            if gcd(x, phi) == 1:
+                return x
 
     def gcd(a, b):
         while b:
             a, b = b, a % b
         return a
 
-    def get_primes(length):
-        start = 2 ** (length - 1)
-        stop = 2 ** (length - 1) + (2 ** (length - 2))
-        return random.sample(list(filter(lambda x: x > start, util.get_primes_limit(stop))), 2)
-
-    p, q = get_primes(length)
+    p, q = util.get_primes(length)
     n = p * q
     phi = (p - 1) * (q - 1)
     e = get_e(phi)
-    d = get_d(e, phi)
+    d = util.modular_inverse(e, phi)
 
     return PublicKey(e, n), PrivateKey(d, n)
--- util.py
+++ util.py
@@ -1,29 +1,7 @@
-import math
+import random
+import sys
 
-
-# Sieve of Atkin
-def get_primes_limit(limit):
-    primes = [2, 3]
-    sieve = [False] * (limit + 1)
-    for x in range(1, int(math.sqrt(limit)) + 1):
-        for y in range(1, int(math.sqrt(limit)) + 1):
-            n = 4 * x ** 2 + y ** 2
-            if n <= limit and (n % 12 == 1 or n % 12 == 5):
-                sieve[n] = not sieve[n]
-            n = 3 * x ** 2 + y ** 2
-            if n <= limit and n % 12 == 7:
-                sieve[n] = not sieve[n]
-            n = 3 * x ** 2 - y ** 2
-            if x > y and n <= limit and n % 12 == 11:
-                sieve[n] = not sieve[n]
-    for x in range(5, int(math.sqrt(limit))):
-        if sieve[x]:
-            for y in range(x ** 2, limit + 1, x ** 2):
-                sieve[y] = False
-    for p in range(5, limit):
-        if sieve[p]:
-            primes.append(p)
-    return primes
+sys.setrecursionlimit(2**12)
 
 
 def power(g_base, a, p_mod):
@@ -37,15 +15,65 @@ def power(g_base, a, p_mod):
     return x % p_mod
 
 
-def sundaram(limit):
-    numbers = list(range(3, limit + 1, 2))
-    half = limit // 2
-    initial = 4
+def is_probable_prime(n):
+    assert n >= 2
 
-    for step in range(3, limit + 1, 2):
-        for i in range(initial, half, step):
-            numbers[i - 1] = 0
-        initial += 2 * (step + 1)
+    if n == 2:
+        return True
 
-        if initial > half:
-            return [2] + list(filter(None, numbers))
+    if n % 2 == 0:
+        return False
+
+    s = 0
+    d = n - 1
+    while True:
+        quotient, remainder = divmod(d, 2)
+        if remainder == 1:
+            break
+        s += 1
+        d = quotient
+    assert (2 ** s * d == n - 1)
+
+    def try_composite(a):
+        if pow(a, d, n) == 1:
+            return False
+        for i in range(s):
+            if pow(a, 2 ** i * d, n) == n - 1:
+                return False
+        return True
+
+    for i in range(5):
+        a = random.randrange(2, n)
+        if try_composite(a):
+            return False
+
+    return True
+
+
+def get_primes(length):
+    start = 2 ** (length - 1)
+    stop = 2 ** length - 1
+    counter = 0
+    primes = []
+    while len(primes) < 2:
+        num = random.randint(start, stop)
+        if is_probable_prime(num):
+            counter += 1
+            primes.append(num)
+    return primes
+
+
+def egcd(a, b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        g, y, x = egcd(b % a, a)
+        return g, x - (b // a) * y, y
+
+
+def modular_inverse(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
