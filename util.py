7e1e36627440f9abf426d962403ba7f39140527a
Roman Gusev <rgusev@hwdtech.com>
2016-03-31 20:04:55 +0600

    Implement some rsa algorithm

---
+++ util.py
@@ -0,0 +1,51 @@
+import math
+
+
+# Sieve of Atkin
+def get_primes_limit(limit):
+    primes = [2, 3]
+    sieve = [False] * (limit + 1)
+    for x in range(1, int(math.sqrt(limit)) + 1):
+        for y in range(1, int(math.sqrt(limit)) + 1):
+            n = 4 * x ** 2 + y ** 2
+            if n <= limit and (n % 12 == 1 or n % 12 == 5):
+                sieve[n] = not sieve[n]
+            n = 3 * x ** 2 + y ** 2
+            if n <= limit and n % 12 == 7:
+                sieve[n] = not sieve[n]
+            n = 3 * x ** 2 - y ** 2
+            if x > y and n <= limit and n % 12 == 11:
+                sieve[n] = not sieve[n]
+    for x in range(5, int(math.sqrt(limit))):
+        if sieve[x]:
+            for y in range(x ** 2, limit + 1, x ** 2):
+                sieve[y] = False
+    for p in range(5, limit):
+        if sieve[p]:
+            primes.append(p)
+    return primes
+
+
+def power(g_base, a, p_mod):
+    x = 1
+    bits = "{0:b}".format(a)
+    for i, bit in enumerate(bits):
+        if bit == '1':
+            x = (((x ** 2) * g_base) % p_mod)
+        elif bit == '0':
+            x = ((x ** 2) % p_mod)
+    return x % p_mod
+
+
+def sundaram(limit):
+    numbers = list(range(3, limit+1, 2))
+    half = limit//2
+    initial = 4
+
+    for step in range(3, limit+1, 2):
+        for i in range(initial, half, step):
+            numbers[i-1] = 0
+        initial += 2*(step+1)
+
+        if initial > half:
+            return [2] + list(filter(None, numbers))
